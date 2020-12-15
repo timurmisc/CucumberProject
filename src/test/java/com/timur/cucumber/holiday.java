f6859f69dad04e055731f2489f384dbd22a7cd5b
Tim Mir <timurmisc@gmail.com>
2020-12-15 16:43:30 -0500

    updated step def file

--- src/test/java/com/timur/cucumber/holiday.java
+++ src/test/java/com/timur/cucumber/holiday.java
@@ -1,14 +1,21 @@
 package com.timur.cucumber;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
 
 public class holiday {
 	
 	@Given ("I book a flight on for air ticket")
 	public void I_book_a_flight_on_for_air_ticket () {
 			
-		System.out.println("hello from GIVEN");
-		
+		System.out.println("hello from GIVEN");	
 	}
 
+	
+	@When ("I go search page")
+	public void  I_go_search_page () {
+		
+		System.out.println("hello from When");
+	}
+	
 }
