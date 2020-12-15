c57fad0f7bc67be14e4d815695ac65ce5157ff88
Tim Mir <timurmisc@gmail.com>
2020-12-15 16:41:11 -0500

    new step def file

---
+++ src/test/java/com/timur/cucumber/holiday.java
@@ -0,0 +1,14 @@
+package com.timur.cucumber;
+
+import cucumber.api.java.en.Given;
+
+public class holiday {
+	
+	@Given ("I book a flight on for air ticket")
+	public void I_book_a_flight_on_for_air_ticket () {
+			
+		System.out.println("hello from GIVEN");
+		
+	}
+
+}
